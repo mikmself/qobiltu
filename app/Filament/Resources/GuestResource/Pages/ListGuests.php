7170a3c041b1c64c6f17c146b3a3ffb7905a7f14
mikmself <mikmself@gmail.com>
2025-04-16 10:37:10 +0700

    feat : add guest export

--- app/Filament/Resources/GuestResource/Pages/ListGuests.php
+++ app/Filament/Resources/GuestResource/Pages/ListGuests.php
@@ -7,6 +7,9 @@ use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Konnco\FilamentImport\Actions\ImportField;
 use Konnco\FilamentImport\Actions\ImportAction;
+use pxlrbt\FilamentExcel\Columns\Column;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
+use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
 
 class ListGuests extends ListRecords
 {
@@ -25,6 +28,16 @@ class ListGuests extends ListRecords
                         ->required(),
                     ImportField::make('institute')
                         ->required(),
+                ]),
+            ExportAction::make()
+                ->exports([
+                    ExcelExport::make()
+                        ->fromTable()
+                        ->withFilename(fn ($resource) => $resource::getModelLabel() . '-' . date('Y-m-d'))
+                        ->withWriterType(\Maatwebsite\Excel\Excel::CSV)
+                        ->withColumns([
+                            Column::make('updated_at'),
+                        ])
                 ]),
         ];
     }
