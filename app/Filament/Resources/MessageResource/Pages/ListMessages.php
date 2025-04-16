ecf67bd9258a06ccce5040090d445d5104e79e55
mikmself <mikmself@gmail.com>
2025-04-16 10:37:58 +0700

    feat : add message export

--- app/Filament/Resources/MessageResource/Pages/ListMessages.php
+++ app/Filament/Resources/MessageResource/Pages/ListMessages.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\MessageResource\Pages;
 use App\Filament\Resources\MessageResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
+use pxlrbt\FilamentExcel\Columns\Column;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
 
 class ListMessages extends ListRecords
 {
@@ -14,6 +17,16 @@ class ListMessages extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            ExportAction::make()
+                ->exports([
+                    ExcelExport::make()
+                        ->fromTable()
+                        ->withFilename(fn ($resource) => $resource::getModelLabel() . '-' . date('Y-m-d'))
+                        ->withWriterType(\Maatwebsite\Excel\Excel::CSV)
+                        ->withColumns([
+                            Column::make('updated_at'),
+                        ])
+                ]),
         ];
     }
 }
